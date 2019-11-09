10f327c9dda9dc9dcfb9500e23fbd7cecaf3aa5c
Bartek Błęcki <bartek@blecki.pl>
2019-11-09 15:55:39 +0100

    debug recordsReducer.js

--- src/reducers/recordsReducer.js
+++ src/reducers/recordsReducer.js
@@ -1,16 +1,16 @@
-import { ADD_RECORD, GET_RECORDSS } from '../actions/actionsrecords';
+import { ADD_RECORD, GET_RECORDS } from '../actions/actionsrecords';
 import recordsData from '../data/records.json'
 
 const initialState = {
   records: recordsData,
 };
 
-const recordsReducer = (state=initialstate, action) => {
+const recordsReducer = (state=initialState, action) => {
   switch(action.type) {
     case ADD_RECORD:
       return{...state, records:[...state.records, action.newRecord]}
 
-    case GET_RECORDSS:
+    case GET_RECORDS:
       console.log(state);
       return Object.assign({}, state, {records: state.records});
   }
