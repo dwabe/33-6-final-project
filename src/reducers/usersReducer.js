93268a231a60ea541c5964bd858b1a12aced0c70
Bartek Błęcki <bartek@blecki.pl>
2019-11-10 09:47:51 +0100

    add usersReducer.js

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,30 @@
+import { ADD_USER, GET_USERS } from '../actions/actionsUsers';
+
+const usersArray = [
+  {
+    id: 1,
+    name: "Bebe",
+    role: "admin"
+  },
+  {
+    id: 2,
+    name: "Lola",
+    role: "user"
+  }
+]
+
+const initialState = {
+  users: usersArray,
+};
+
+const usersReducer = (state=initialState, action) => {
+  switch(action.type) {
+    case ADD_USER:
+      return {...state, users:[...state.users, action.newUser]}
+    case GET_USERS:
+      return Object.assign({}, state, {users: state.users});
+  }
+      return state;
+};
+
+export default usersReducer;
