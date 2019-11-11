ed0bd2c128a0ee2c0f0a0a421a225097d4f2a6d0
Bartek Błęcki <bartek@blecki.pl>
2019-11-11 22:43:32 +0100

    update main.index.js

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,39 +1,141 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
-import { getRecords, addRecord } from '../actions/actionsRecords';
-import RecordsList from "./Records/RecordsList";
+import ProductsList from "../Products/ProductsList";
+import Filter from "../Sidebar/Sidebar";
+import { Container } from 'reactstrap';
+
+require('./main.css');
 
 class Main extends Component {
   constructor(props) {
-    super(props);
+		super(props);
+
+		this.state = { 
+			sort: '',
+			genre: '',
+			type: '',
+			search: '',
+			products: [], 
+			filteredProducts: []
+		};
+		this.handleSearchProducts = this.handleSearchProducts.bind(this);
+		this.handlePriceSort = this.handlePriceSort.bind(this);
+		this.handleArtistSort = this.handleArtistSort.bind(this);
+		this.handleGenreFilter = this.handleGenreFilter.bind(this);
+		this.handleTypeFilter = this.handleTypeFilter.bind(this);
+	}
+
+	componentDidMount() {	
+    this.setState({
+      products: this.props.products.products,
+    })
+    this.listProducts();
   }
 
-  componentDidMount() {
-    this.props.getRecords();
-  }
+	listProducts() {
+		this.setState(state => {
+			if (state.sort !== '') {
+				state.products.sort((a, b) =>
+				(state.sort === 'lowest'
+					? ((a.price > b.price) ? 1 : -1)
+					: ((a.price < b.price) ? 1 : -1)));
+			} else {
+				state.products.sort((a, b) => (a.id > b.id) ? 1 : -1);
+			}
+			let tempList = state.products;
+			if (state.search !== '') {
+				tempList = state.products.filter(product =>  product.title.toLowerCase().includes(state.search.toLowerCase()));
+				if (state.genre !== '') {
+					tempList = tempList.filter(a => a.genre === state.genre) ;
+				}
+				if (state.type !== '') {
+					tempList = tempList.filter(a => a.type.toLowerCase() === state.type.toLowerCase()) ;
+				}
+			} else {
+				if (state.genre !== '') {
+					tempList = state.products.filter(a => a.genre === state.genre) ;
+				}
+				if (state.type !== '') {
+					tempList = state.products.filter(a => a.type.toLowerCase() === state.type.toLowerCase()) ;
+				}
+			}
+			return { filteredProducts: tempList };
+		})
+	}
 
-  render() {
-    console.log(this.props)
-      return(
-        <div>
-          <div className="sidebar">Sidebar</div>
-            <RecordsList records={this.props.records.records} />
-        </div>
-      );
-  }
-};
+	handleSearchProducts(event) {
+		this.setState({search: event.target.value});
+		this.listProducts();
+	}
 
-const mapStateProps = function(store) {
-  return {
-    records : store.recordsReducer,
-  };
+  handlePriceSort(event) {
+		this.setState({sort: event.target.value});
+		this.listProducts();
+	}
+
+	handleArtistSort(event) {
+		this.setState({sort: event.target.value});
+		this.listProducts();
+	}
+
+	handleGenreFilter(event) {
+		this.setState({genre: event.target.value});
+		this.listProducts();
+	}
+
+	handleTypeFilter(event) {
+		this.setState({type: event.target.value});
+		this.listProducts();
+	}
+
+	render() {
+		return(   
+			<div className="homepage-container">
+				<div className="sidebar">
+					<Filter 
+						genre={this.state.genre} 
+						type={this.state.type} 
+						sort={this.state.sort} 
+						search={this.state.search} 
+						handlePriceSort={this.handlePriceSort}
+						handleArtistSort={this.handleArtistSort}
+						handleGenreFilter={this.handleGenreFilter} 
+						handleTypeFilter={this.handleTypeFilter} 
+						handleSearchProducts={this.handleSearchProducts}
+					/>
+				</div>
+				<Container>
+					<ProductsList 
+						products={this.state.filteredProducts}  
+						addToCart={this.props.addToCart}
+						removeFromCart={this.props.removeFromCart}
+						cart={this.props.cart}
+					/>
+				</Container>
+      </div>
+		);
+	}
+} 
+
+const mapStateToProps = function(store) {
+	return {
+		records: store.recordsReducer,
+		products: store.productsReducer,
+		cart: store.cart,
+		
+	};
 };
 
 const mapDispatchToProps = dispatch => ({
-  getRecords: () => dispatch(getRecords())
-})
+  addToCart: (item) => {
+		dispatch({type: 'ADD_ITEM', payload: item})
+	},
+	removeFromCart: (item) => {
+		dispatch({type: 'REMOVE_ITEM', payload: item})
+	}
+});
 
-export default connect (
-  mapStateProps,
-  mapDispatchToProps,
+export default connect(
+  mapStateToProps,
+	mapDispatchToProps
 )(Main);
