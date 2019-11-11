da0b254a9f9f6ca6a2ee5fb20b27948819166fa5
Bartek Błęcki <bartek@blecki.pl>
2019-11-11 22:54:21 +0100

    update Router.js

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Main from './components/Main';
-import User from './components/User';
+import  Main  from './components/Main/index';
+import  User  from './components/User/User';
+import  ProductDetails  from './components/Products/ProductDetails';
 import { NoMatch } from './components/NoMatch';
-import { Cart } from './components/Cart';
+import  Cart  from './components/Cart/index';
+import  Checkout  from './components/Checkout/Checkout';
+import  SummaryPage  from './components/SummaryPage/SummaryPage';
 
 const Router = () => (
-  <Switch>
-    <Route exact path='/' component={Main} />
-    <Route exact path='/cart' component={Cart} />
-    <Route exact path='/User' component={User} />
-    <Route path='*' exact={true} component={NoMatch} />
-  </Switch>
+    <Switch>
+        <Route exact path='/' component={Main} />
+        <Route path="/product/:id" component={ProductDetails} />
+        <Route exact path='/cart' component={Cart} />
+        <Route exact path='/checkout' component={Checkout} />
+        <Route exact path='/summary' component={SummaryPage} />
+        <Route exact path='/User' component={User} />
+        <Route  path='*' exact={true} component={NoMatch} />
+    </Switch>
 )
 
 export default Router;
