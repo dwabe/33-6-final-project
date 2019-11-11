49eb14287fbe4b60e0ec55d5cc9fa7b52c2944ff
Bartek Błęcki <bartek@blecki.pl>
2019-11-11 22:45:33 +0100

    update reducers index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
-import usersReducer from './usersReducer';
-import recordsReducer from './recordsReducer'; +
+import usersReducer  from './usersReducer';
+import productsReducer  from './productsReducer';
+import cartReducer  from './cartReducer';
+import checkoutReducer  from './checkoutReducer';
 
 const reducers = combineReducers({
-  usersReducer: usersReducer,
-  recordsReducer: recordsReducer
+    usersReducer: usersReducer,
+    productsReducer: productsReducer,
+    cart: cartReducer,
+    checkout: checkoutReducer
 });
 
 export default reducers;
