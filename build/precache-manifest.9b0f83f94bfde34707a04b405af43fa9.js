65a5ed6ea74cb9480f66682bbfc92277677503e0
Bartek Błęcki <bartek@blecki.pl>
2019-11-12 22:12:11 +0100

    add build ver

---
+++ build/precache-manifest.9b0f83f94bfde34707a04b405af43fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a849c8ba131604203d8d6328c1ff2c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29abfa1a9a8c940bbd0a",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "8f3fc79142a7e367b5c8",
+    "url": "/static/css/main.ab511b5b.chunk.css"
+  },
+  {
+    "revision": "29abfa1a9a8c940bbd0a",
+    "url": "/static/js/2.9c0dcde7.chunk.js"
+  },
+  {
+    "revision": "8f3fc79142a7e367b5c8",
+    "url": "/static/js/main.6ff89deb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
